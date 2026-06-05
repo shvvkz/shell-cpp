a36015d4309ae54958db7e72e3ff5bfbe3afc357
shvvkz <viggocasciano0301@gmail.com>
2026-06-05 20:46:21 +0200

    add ps1

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+#include <filesystem>
+#include <string>
 #include "parser.hpp"
 using namespace std;
 
+string ps1() {
+    string current_path = filesystem::current_path();
+    string final_ps1 = current_path + "\n> ";
+    return final_ps1;
+}
 
 int main() {
     while (1) {
         string user_input;
-        std::cout << "Type: ";
+        std::cout << ps1();
         std::getline(std::cin, user_input);
         parse_input(user_input); 
     }
